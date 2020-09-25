07151dc29ee596ad5eaee0406bb4d8fb36c62cc8
Alex Cavazos <me@alexcavazos.com>
2020-09-25 15:29:58 -0500

    Initial Commit

---
+++ SayuriOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SayuriOS
+//
+//  Created by Alex Cavazos on 25/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SayuriOS/SayuriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayuriOSApp.swift
+//  SayuriOS
+//
+//  Created by Alex Cavazos on 25/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SayuriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
